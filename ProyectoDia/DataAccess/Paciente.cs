2696808d72214f6bcc25caf4ab8b75c9acaa0515
Candela Martinez <cma@novicell.es>
2022-05-30 17:36:04 +0200

    CodeFirst

---
+++ ProyectoDia/DataAccess/Paciente.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ProyectoDia.DataAccess
+{
+    public class Paciente
+    {
+        [Key]
+        public int TarjetaSanitaria { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        private String Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        private String Apellido { get; set; }
+        private DateTime FechaNacimiento { get; set; }
+        private String Telefono { get; set; }
+        private String Direccion { get; set; }
+        private IEnumerable<VisitaMedica> ListaVisitasMedicas { get; set; }
+        private Medico MedicoCabecera { get; set; }
+    }
+}
